7fa9a52d7dafd6cfa812d887a1404f3a1862a16f
vagrant <vagrant@homestead>
2019-06-22 14:30:27 +0000

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,22 @@ class UsersController extends Controller
     public function show(User $user){
         return view('users.show', compact('user'));
     }
+
+    //创建用户
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
+    }
 }
